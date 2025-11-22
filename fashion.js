ad60519ee064263c9fc3b8c1c420b7d1eda2dd95
RikkeSHu <111729304+RikkeSHu@users.noreply.github.com>
2025-11-22 12:42:23 +0100

    fashion
    Added css, js and things in html

--- fashion.js
+++ fashion.js
@@ -1,16 +1,28 @@
 const sections = document.querySelectorAll('.scroll-section');
+const images = document.querySelectorAll('.scroll-image-left');
+const texts = document.querySelectorAll('.scroll-text-right');
 
-function revealSections() {
+function revealElements() {
     const triggerBottom = window.innerHeight * 0.85;
+
     sections.forEach(section => {
         const sectionTop = section.getBoundingClientRect().top;
-        if(sectionTop < triggerBottom) {
-            section.classList.add('active');
-        } else {
-            section.classList.remove('active');
-        }
+        if(sectionTop < triggerBottom) section.classList.add('active');
+        else section.classList.remove('active');
+    });
+
+    images.forEach(img => {
+        const imgTop = img.getBoundingClientRect().top;
+        if(imgTop < triggerBottom) img.classList.add('active');
+        else img.classList.remove('active');
+    });
+
+    texts.forEach(text => {
+        const textTop = text.getBoundingClientRect().top;
+        if(textTop < triggerBottom) text.classList.add('active');
+        else text.classList.remove('active');
     });
 }
 
-window.addEventListener('scroll', revealSections);
-window.addEventListener('load', revealSections);
+window.addEventListener('scroll', revealElements);
+window.addEventListener('load', revealElements);
